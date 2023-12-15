31ead69cbb0b1e7409513b406ee0f73ef7a6d7b1
Luis Carlos <carlosrojocai@hotmail.com>
2023-12-15 12:15:29 -0500

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import NavBar from "./components/NavBar/NavBar"
+import SectionA from "./components/sectionA/SectionA"
+
+
+function App() {
+
+  return (
+    <>
+    <NavBar />
+    <SectionA />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,16 @@
+import classes from './NavBar.module.css'
+
+const NavBar = () => {
+    return (
+        <nav className={classes.container}>
+            <h1 className= {classes.colorRed}>colombia</h1>
+            <h1>venezuela</h1>
+            <h1>colombia</h1>
+            <h1>venezuela</h1>
+            <h1>colombia</h1>
+            <h1>venezuela</h1>
+        </nav>
+    )
+} 
+
+export default NavBar
