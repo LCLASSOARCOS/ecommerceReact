6a18ce92f16cf5df684194159b3c9f22ec89aa80
Luis Lasso <carlosrojocai@gmail.com>
2024-02-13 23:32:11 -0500

    responsive

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,15 +1,25 @@
 import classes from "./NavBar.module.css";
 import CartWidget from '../CartWidget/CartWidget'
 import { Link } from 'react-router-dom';
+import { useState } from "react";
 
 const NavBar = () => {
-  
+   const [menuOpen, setMenuOpen] = useState(false)
+   
+   const toggleMenu = () => {
+      setMenuOpen(!menuOpen)
+   }
+
     return (
-        <header>
+   <header>
 
       <nav className={classes.navegation}>
 
-         <ul className={classes.navCategoria}>
+         <div className={classes.menuIcon} onClick={toggleMenu}>
+            ☰
+         </div>
+
+         <ul className={`${classes.navCategoria} ${menuOpen ? classes.menuOpen : ""}`}>
             <Link to={'/category/hombre'} className={classes.botonesNav}>Hombre</Link>
             <Link to={'/category/mujer'} className={classes.botonesNav}>Mujer</Link>
             <Link to={'/category/sale'} className={classes.botonesNav}>Sale</Link>
